a5b0324d8a4af728499e41b566253b65fbdd91de
Dejan Jacimovic <dejan.jacimovic@stuntcoders.com>
2017-03-21 15:40:48 +0100

    Add further setup automation for iTerm

--- after_install.sh
+++ after_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Remove apps from Dock
 dockutil --remove 'Launchpad' --allhomes
 dockutil --remove 'Siri' --allhomes
 dockutil --remove 'Safari' --allhomes
@@ -15,7 +16,21 @@ dockutil --remove 'iBooks' --allhomes
 dockutil --remove 'App Store' --allhomes
 dockutil --remove 'System Preferences' --allhomes
 
+# Add apps to Dock
 dockutil --add '/Applications/Google Chrome.app'/ --position 2 --allhomes
 dockutil --add '/Applications/PhpStorm.app' --position 3 --allhomes
 dockutil --add '/Applications/Sublime Text.app'/ --position 4 --allhomes
 dockutil --add '/Applications/iTerm.app' --position 5 --allhomes
+
+# Install Grunt and Sass
+npm install --global grunt-cli
+sudo gem install sass
+
+# Install yadr
+sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
+cd ~/.yadr/ && rake update
+sed -i '' -e 's/skwp/agnoster/g' ~/.yadr/zsh/theme.zsh
+
+# Install theme
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sed -i '' -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
