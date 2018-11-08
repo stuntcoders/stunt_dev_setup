173da9aa347c30f37fc318eba2d2d61afae5670b
Dejan Jacimovic <dejanjacimovic@users.noreply.github.com>
2018-11-08 23:24:34 +0100

    Add alias for bat to replace cat

--- after_install.sh
+++ after_install.sh
@@ -37,3 +37,6 @@ sed -i '' -e 's/skwp/agnoster/g' ~/.yadr/zsh/theme.zsh
 # Install theme
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sed -i '' -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+
+# Add aliases
+echo "alias cat='bat'" >> ~/.zshrc
