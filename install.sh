03984d61237894007fb3481577a79b9a04326e83
Dejan Jacimovic <dejanjacimovic@users.noreply.github.com>
2018-11-08 21:58:21 +0100

    Add installation of https://github.com/sharkdp/bat

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install caskroom/cask/brew-cask
+brew install bat
 
 brew tap homebrew/bundle
 brew bundle
