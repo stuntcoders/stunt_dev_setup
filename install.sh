5b464f9b08b98607b3c44add8e689cd6ff4305e5
Dejan Jacimovic <dejanjacimovic@users.noreply.github.com>
2022-04-28 12:23:42 +0200

    Add brew to path

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 xcode-select --install
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew tap homebrew/bundle
 brew bundle
